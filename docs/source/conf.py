2fb4c52b5320762aee9c8fb89cbfbd3b9443bb16
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-03-02 13:16:38 +0100

    Edit to doc files for stabalization

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,7 +68,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = [".DS_Store", "**.ipynb_checkpoints"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
