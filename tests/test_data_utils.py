f78e58e279fdbfe9f5ad54aa6bca8d3e4a193cfc
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-03-10 18:28:51 +0100

    Add tqdm to long cleaning steps and fix docs

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -14,50 +14,47 @@ def test_input_conversion_dict():
 
 
 def test__combine_tokens_to_str():
-    texts = ["words", "to", "not", "be", "combined"]
-    texts_lol = [["words", "to"], ["not"], ["be", "combined"]]
-    ignore_words = ["not"]
-    result_ignore_not = "words to be combined"
-    result_not = "words to not be combined"
+    texts = ["words", "to", "be", "combined"]
+    texts_lol = [["words", "to"], ["be", "combined"]]
+    result = "words to be combined"
 
-    assert (
-        data_utils._combine_tokens_to_str(texts=texts, ignore_words=ignore_words)
-        == result_ignore_not
-    )
+    assert data_utils._combine_tokens_to_str(texts=texts) == result
 
-    assert (
-        data_utils._combine_tokens_to_str(texts=texts_lol, ignore_words=ignore_words)
-        == result_ignore_not
-    )
-
-    assert (
-        data_utils._combine_tokens_to_str(texts=texts, ignore_words=None) == result_not
-    )
-
-    assert (
-        data_utils._combine_tokens_to_str(texts=texts_lol, ignore_words=None)
-        == result_not
-    )
+    assert data_utils._combine_tokens_to_str(texts=texts_lol) == result
 
 
-def test_lower_remove_unwanted():
-    assert data_utils.lower_remove_unwanted(
-        args=(["Harry", "Potter", "25", "Zoo"], True)
+def test__lower_remove_unwanted():
+    assert data_utils._lower_remove_unwanted(
+        args=(
+            ["Harry", "Potter", "25", "Zoo", "remove_please"],
+            True,
+            ["remove_please"],
+        )
     ) == ["potter", "zoo"]
 
-    assert data_utils.lower_remove_unwanted(
-        args=(["Harry", "Potter", "25", "Zoo"], False)
+    assert data_utils._lower_remove_unwanted(
+        args=(
+            ["Harry", "Potter", "25", "Zoo", "remove_please"],
+            False,
+            ["remove_please"],
+        )
     ) == ["harry", "potter", "zoo"]
 
 
-def test_lemmatize():
+def test__lemmatize():
     try:
         nlp = spacy.load("en")
     except:
         os.system("python -m spacy download {}".format("en"))
         nlp = spacy.load("en")
-    assert data_utils.lemmatize([["better"], ["walking"], ["dogs"]], nlp=nlp) == [
+    assert data_utils._lemmatize([["better"], ["walking"], ["dogs"]], nlp=nlp) == [
         ["well"],
         ["walk"],
         ["dog"],
     ]
+
+
+def test__subset_and_combine_tokens():
+    assert data_utils._subset_and_combine_tokens(
+        args=([0, ["here", "are", "some", "tokens", "and", "extras"]], 4)
+    ) == [0, "here are some tokens"]
